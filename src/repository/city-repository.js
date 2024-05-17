48aaaad7bfae8a889d64af8947388f2ff7f2a2a6
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-05-17 13:58:10 +0530

    completed cityrepository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,10 +1,12 @@
+const { where } = require("sequelize");
 const { City } = require("../models/index.js");
 
 class CityRepository {
   async createCity({ name }) {
+    // accepting an object
     try {
       const city = await City.create({
-        name,
+        name: name, // or name,
       });
       return city;
     } catch (error) {
@@ -21,6 +23,30 @@ class CityRepository {
         },
       });
       return true;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPK(cityId);
+      return city;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+      return city;
     } catch (error) {
       console.log("Something went wrong in the repository layer");
       throw { error };
